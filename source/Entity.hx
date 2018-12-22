f1b2772f4c4a74c11e53993439bed00d846c0914
rapucadyil <44212572+rapucadyil@users.noreply.github.com>
2018-12-22 11:35:24 +0100

    Developed entity more...

--- source/Entity.hx
+++ source/Entity.hx
@@ -7,19 +7,28 @@ import flixel.FlxG;
 
 class Entity extends FlxSprite {
 
+    /* Velocity variables for this entity */
     public var velX : Float;
     public var velY : Float;
+
+    /* Health variables for this entity */
+    public var currHealth : Float;
+    public var maxHealth : Float;
 
     /**
      * Creates a new Entity
      * @param X : the X position to place this entity
      * @param Y : the Y position to place this entity
      */
-    public function new (?X:Float=0, ?Y:Float=0) {
+    public function new (?X:Float=0, ?Y:Float=0, ?CH:Float=0, ?MH:Float=0) {
         super(X,Y);
+        /* This is used for scaling */
         scale.set(0.25, 0.25);
         loadGraphic(AssetPaths.player__png);
-        FlxTween.tween(scale, { x:0.25, y:0.25 }, 0.4, { ease:FlxEase.elasticOut } );
+        /* This is used for scaling */
+        FlxTween.tween(scale, { x:0.25, y:0.25 }, 0.4, { ease:FlxEase.elasticOut });
+        this.currHealth = CH;
+        this.maxHealth = MH;
         this.x = X;
         this.y = Y;
         velX = 200;
@@ -45,6 +54,18 @@ class Entity extends FlxSprite {
                 this.x = this.x + X;
                 this.flipX = false;
             }
+        }
+    }
+    
+    /**
+     * Modifies the health attribute of this entity
+     * @param order : determines if the max or current health value is gonna be modded
+     * @param value : the value to change the ordered attribute by
+     */
+    public function modifyHealth(?order:String, ?value:Float=0) {
+        switch(order) {
+            case "MAX": this.maxHealth = this.maxHealth += value;
+            case "CURR": this.currHealth = this.currHealth += value;
         }
     }
 
